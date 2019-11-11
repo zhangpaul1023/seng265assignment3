3da9ac6045ee6b12070085b0fc45e4f7c4e81a52
Adam Murray <admin@armurray.com>
2019-11-10 22:59:13 -0800

    initial commit: added assignment description, tools, tests and starter-file

---
+++ OLAP.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import csv
+
+
+def main():
+    # deliberately left blank for your implementation - remove this comment and begin
+    
+
+if __name__ == '__main__':
+    main()
